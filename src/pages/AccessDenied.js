eef21888a4d0098c4b753f2c05353a300ff318dc
Andres Fonseca <andresmfonseca@outlook.com>
2021-11-29 15:47:47 -0500

    Cambio a login para ser responsive Otros archivos agregados para pruebas (navbar, sidenavbar, sidebar2)

--- src/pages/AccessDenied.js
+++ src/pages/AccessDenied.js
@@ -2,12 +2,12 @@ import {Link} from 'react-router-dom';
 
 function AccessDenied() {
     return (
-        <div>
+        <div className="p-4">
             <h1>Access Denied</h1>
             <p>You do not have permission to access this page.</p>
             {/* Boton para regresar */}
             <Link to="/">
-                <input type="button" className="btn btn-primary" value="Back" />
+                <input type="button" className="btn btn-primary m-2" value=" <=  Back  " />
                     
                 
             </Link>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,22 +5,22 @@ import Logo from './logo.jpeg';
 import { Link } from "react-router-dom";
 function Login() {
   return (
-    <section className="row form-02-main">
-      
-   
-          <div className="col-md-3">
-            <div className="form-02-left">
-              <img src={Logo} alt="logo" style={{ width: "150px", margin: "20%"}} />
-            </div>
-          </div>
-      
-      
-      <div className="col-md-3 container" style={{ marginLeft: "30%" }}>
+    <section className="row form-02-main p-3">
+
+
+      <div className="col-md-6">
+        <div className="p-3">
+          <img src={Logo} alt="logo" style={{ width: "150px"}} className="img-rounded"/>
+        </div>
+      </div>
+
+
+      <div className="col-md-6 container" >
         <div className="row">
           <div className="col-md-12">
             <div className="_lk_de">
               <div className="form-03-main">
-             
+
                 <h2>Bienvenido</h2>
                 <div className="form-group">
                   <input type="email" name="email" className="form-control _ge_de_ol" placeholder="Enter Email" required="" aria-required="true" />
@@ -31,7 +31,7 @@ function Login() {
                 </div>
 
                 <div className="checkbox form-group">
-          
+
                   <a href="none">¿Olvidaste tu contraseña?</a>
                 </div>
 
