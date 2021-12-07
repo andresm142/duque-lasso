65bc5cc3eeeaad2eabd3f175a2f555a4be3f3adf
Andres Fonseca <andresmfonseca@outlook.com>
2021-12-07 00:04:53 -0500

    Actualizacion

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -18,7 +18,7 @@ function Profile() {
     return (
         <div >
 
-            <div className="container-fluid">
+            <div className="container-fluid container_perfil">
                 <div className="perfil-container">
                     <div className="titulo-imagen">
                         <h1 className="titulo">CONIGURACION DEL PERFIL</h1>
@@ -27,24 +27,24 @@ function Profile() {
                     <div className="perfil-container-form">
                         <form action="">
                             <div className="form-group1">
-                                <div className="form-group">
+                                <div className="form-group2">
                                     <label htmlFor="nombre_usuario">Nombre</label>
                                     <input type="text" className="form-control" id="nombre_usuario"
                                          placeholder="Nombre" />
                                 </div>
-                                <div className="form-group">
+                                <div className="form-group2">
                                     <label htmlFor="apellido_usuario">Apellido</label>
                                     <input type="text" className="form-control" id="apellido_usuario"
                                          placeholder="Apellido" />
                                 </div>
                             </div>
                             <div className="form-group1">
-                                <div className="form-group">
+                                <div className="form-group2">
                                     <label htmlFor="correo_usuario">Correo</label>
                                     <input type="email" className="form-control" id="correo_usuario"
                                          placeholder="Correo" />
                                 </div>
-                                <div className="form-group">
+                                <div className="form-group2">
                                     <label htmlFor="telefono_usuario">Telefono</label>
                                     <input type="number" className="form-control" id="telefono_usuario"
                                          placeholder="Telefono" />
@@ -52,12 +52,12 @@ function Profile() {
 
                             </div>
                             <div className="form-group1">
-                                <div className="form-group">
+                                <div className="form-group2">
                                     <label htmlFor="contraseña_usuario">Contraseña</label>
                                     <input type="password" className="form-control" id="contraseña_usuario"
                                          placeholder="Contraseña" />
                                 </div>
-                                <div className="form-group">
+                                <div className="form-group2">
                                     <label htmlFor="confirmar_contraseña_usuario">Confirmar Contraseña</label>
                                     <input type="password" className="form-control" id="confirmar_contraseña_usuario"
                                          placeholder="Confirmar Contraseña" />
